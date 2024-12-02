a4c5724cebf38410e61775c1e46d2c0012109481
Nisula Premaratne <nisula.rap@gmail.com>
2024-12-02 17:07:11 +0530

    Updated all file

--- backend/app/app.js
+++ backend/app/app.js
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
+// Test Route
+app.get('/', (req, res) => {
+  res.send('Hello, World! Server is working fine.');
+});
+
 export default app;
